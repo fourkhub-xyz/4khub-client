b88e0fa0bf9dc09f27362cf6a6dff8d7c3213860
fourkhub-xyz <144124975+fourkhub-xyz@users.noreply.github.com>
2023-11-08 16:37:49 +0800

    'add_explore_history'

--- web/static/components/layout/navbar/index.js
+++ web/static/components/layout/navbar/index.js
@@ -8,11 +8,23 @@ import { CustomElement } from "../../utility/utility.js";
 // : 显示别名 (可选)
 const navbar_list = [
   {
-    name: "我的媒体库",
-    page: "index",
-    icon: html`
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="5 12 3 12 12 3 21 12 19 12"></polyline><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path></svg>
-    `,
+    name: "我的影音",
+    list: [
+        {
+          name: "我的媒体库",
+          page: "index",
+          icon: html`
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="5 12 3 12 12 3 21 12 19 12"></polyline><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path></svg>
+          `,
+        },
+        {
+          name: "浏览历史",
+          page: "media_explore_history",
+          icon: html`
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-history" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 8l0 4l2 2"></path><path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5"></path></svg>
+          `,
+        },
+    ]
   },
   {
     name: "会员中心",
