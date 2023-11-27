89d5d29d33b70c7cc5407c9f8513fdfd2faa261b
fourkhub-xyz <144124975+fourkhub-xyz@users.noreply.github.com>
2023-11-27 17:12:56 +0800

    'change_telegram'

--- web/static/components/page/mediainfo/index.js
+++ web/static/components/page/mediainfo/index.js
@@ -183,7 +183,7 @@ export class PageMediainfo extends CustomElement {
                                   <span class="btn btn-primary btn-pill me-1"
                                     @click=${(e) => {
                                         e.stopPropagation();
-                                        media_search(this.tmdbid + "", this.media_info.title, this.media_type,this.subtype);
+                                        media_search(this.tmdbid + "", this.media_info.title, this.media_type,this.subtype,this.fourkhub_id);
                                     }}>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="10" cy="10" r="7"></circle><line x1="21" y1="21" x2="15" y2="15"></line></svg>
                                     搜索资源
