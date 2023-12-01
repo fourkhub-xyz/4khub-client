8be99645817ebe9bc50e973456b9d2a335482f2c
fourkhub-xyz <144124975+fourkhub-xyz@users.noreply.github.com>
2023-12-01 13:25:41 +0800

    'change_indexer'

--- web/static/components/page/mediainfo/index.js
+++ web/static/components/page/mediainfo/index.js
@@ -179,7 +179,7 @@ export class PageMediainfo extends CustomElement {
                               `
                         : html`
                                   <!-- 免费的展示 不需要改动，去掉搜索-->
-                                  <!-- 
+                                  
                                   <span class="btn btn-primary btn-pill me-1"
                                     @click=${(e) => {
                                         e.stopPropagation();
@@ -188,7 +188,7 @@ export class PageMediainfo extends CustomElement {
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="10" cy="10" r="7"></circle><line x1="21" y1="21" x2="15" y2="15"></line></svg>
                                     搜索资源
                                   </span>
-                                  -->
+                                  
                                   ${this.fav == "1"
                                   ? html`
                                     <span class="btn btn-pill btn-pinterest"
